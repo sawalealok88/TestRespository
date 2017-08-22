42addb839213d9e6839a0c511f3953b0b8810d02
alok.sawale <alok.sawale@YP1006754DT.Yash.com>
2017-08-22 16:30:02 +0530

    This is a Test Message...

---
+++ Test/src/com/test/MyClass.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class MyClass {
+
+	
+	public static void main(String[] args) {
+		Test test = (msg, a)->{
+				return msg+"  Hello from alok"+ a;
+		};
+		System.out.println(test.vcdgcjsdbbvrh("Hiii", 123));
+		System.out.println(test.vcdgcjsdbbvrh("Hiii", 1234));
+		System.out.println(test.vcdgcjsdbbvrh("Hiii", 1238));
+	}
+}
---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,8 @@
+package com.test;
+
+@FunctionalInterface
+public interface Test {
+	
+	public String vcdgcjsdbbvrh(String msg, int a);
+
+}
